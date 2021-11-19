7fa03455614f6d797d7df4e4d5ddcdc815f579d3
kazurayam <kazuaki.urayama@gmail.com>
2021-11-19 18:04:45 +0900

    change project name: ks_overhead_analysis

--- Keywords/my/Verifier.groovy
+++ Keywords/my/Verifier.groovy
@@ -6,34 +6,33 @@ import com.kms.katalon.core.annotation.Keyword
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 public class Verifier {
-	
+
 	@Keyword
 	static void printID(String id) {
 		WebUI.comment("ID=${id}")
 	}
-	
+
 	@Keyword
 	static void verifySomething(String id) {
-		
+
 		WebUI.comment("start verifySomething with ID=${id}")
-		
+
 		WebUI.openBrowser('')
-		
+
 		WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
-		
+
 		WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/a_Make Appointment'))
-		
+
 		WebUI.verifyElementPresent(findTestObject('Object Repository/Page_CURA Healthcare Service/input_Username_username'), 5)
-		
+
 		WebUI.setText(findTestObject('Object Repository/Page_CURA Healthcare Service/input_Username_username'), 'John Doe')
-		
+
 		WebUI.setEncryptedText(findTestObject('Object Repository/Page_CURA Healthcare Service/input_Password_password'), 'g3/DOGG74jC3Flrr3yH+3D/yKbOqqUNM')
-		
+
 		WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/button_Login'))
-		
+
 		WebUI.verifyElementPresent(findTestObject('Object Repository/Page_CURA Healthcare Service/button_Book Appointment'), 5)
-		
+
 		WebUI.closeBrowser()
-		
 	}
 }
